39d96a76f224b6df5ee4bd8255b937b963e1c80a
Deboracosta.v <csvdebora@gmail.com>
2023-09-26 16:55:28 -0300

    FORMULARIO finalizado

--- cadastro.js
+++ cadastro.js
@@ -1,9 +1,28 @@
 
-var botaoCadrastro = document.querySelector(".enviar");
 
-alert("E-mail Cadastrado com sucesso");
+let email = document.getElementById('email');
+let form = document.querySelector('form');
+let textForm = document.getElementById('textForm');
+let textEmail = document.getElementById('textEmail');
 
 
 
+
+email.addEventListener("keyup", ()=>{
+   if(validaEmail(email.value)!== true){
+      textEmail.textContent = "O formato do E-MAIL deve ser Ex: texto@abc.com"
+   }
+   else {
+      textEmail.textContent = " "
+   }
+   
+})
+
+alert("Obrigado por Participar")
+
+function validaEmail(email) {
+   let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2, 4})$/;
+   return emailPattern.test(email);
+}
 
 
